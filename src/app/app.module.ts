f0559d8b1b7a0595a05c64b1c16047edfcf632bd
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2022-01-26 16:58:51 +0100

    feat:add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ToolbarModule } from './toolbar/toolbar.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule, ToolbarModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
